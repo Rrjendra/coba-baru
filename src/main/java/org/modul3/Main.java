fc582e4222ab5262c3553802a827338fa5028585
ocid40050090 <rrajendraaji@gmail.com>
2023-12-30 10:20:41 +0700

    percobaan

---
+++ src/main/java/org/modul3/Main.java
@@ -0,0 +1,8 @@
+package org.modul3;
+public class Main {
+    public static void main(String[] args) tambah {
+    tambah
+        System.out.println("hellowworld");
+    /** menampilkan katanya
+    }
+}
